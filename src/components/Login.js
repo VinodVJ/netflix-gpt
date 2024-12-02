4a5e701f66d04c17c2fb9ef304dd27bb59a47708
Vinod J <vinodj.2194@gmail.com>
2024-12-02 10:09:37 +0530

    Added form validations

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,34 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { validateSignInForm } from '../utils/validate';
 import Header from './Header'
 
 const Login = () => {
 
     const [isSignInToggle, setIsSignInToggle] = useState(true)
+    const email = useRef(null);
+    const password = useRef(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+
     const toggleSiginIn = () => {
         setIsSignInToggle(!isSignInToggle);
+    }
+
+    const handleSignInClick = () => {
+        const message = validateSignInForm(email.current.value, password.current.value);
+        setErrorMessage(message);
     }
 
     return (
         <div>
             <Header />
+
             <div className='absolute'>
                 <img src='https://assets.nflxext.com/ffe/siteui/vlv3/2bcf01ee-7ef6-4930-b0d5-c6863853c461/web/IN-en-20241125-TRIFECTA-perspective_a47db038-756f-4f26-b1f7-cfc882b98746_large.jpg'
                     alt='background' />
             </div>
-            <form className='absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
+
+            <form onSubmit={(e) => {e.preventDefault()}}
+                className='absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
                 <h1 className='font-bold text-3xl py-4'>
                     {isSignInToggle ? 'Sign In' : 'Sign Up'}
                 </h1>
@@ -23,11 +36,15 @@ const Login = () => {
                     <input type='text' placeholder='Full Name'
                         className='p-4 my-4 w-full bg-gray-700' />
                 )}
-                <input type='text' placeholder='Email Address'
+                <input ref={email} type='text' placeholder='Email Address'
                     className='p-4 my-4 w-full bg-gray-700' />
-                <input type='password' placeholder='Password'
+                <input ref={password} type='password' placeholder='Password'
                     className='p-4 my-4 w-full bg-gray-700' />
-                <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>
+                <p className='text-red-700 font-bold'>
+                    {errorMessage}
+                </p>
+                <button onClick={handleSignInClick}
+                    className='p-4 my-6 bg-red-700 w-full rounded-lg'>
                     {isSignInToggle ? 'Sign In' : 'Sign Up'}
                 </button>
                 <p className='py-6 cursor-pointer'
@@ -35,6 +52,7 @@ const Login = () => {
                     {isSignInToggle ? 'New to Netflix Sign Up now': 'Already a user Sign In now'}
                 </p>
             </form>
+
         </div>
     )
 }
---
+++ src/utils/validate.js
@@ -0,0 +1,9 @@
+export const validateSignInForm = (email, password) => {
+    const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const isPasswordValid = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+
+    if (!isEmailValid) return 'Enter Valid Email ID';
+    if (!isPasswordValid) return 'Kindly enter valid password';
+
+    return null;
+}
