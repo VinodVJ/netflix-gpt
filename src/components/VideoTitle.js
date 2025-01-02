9806084bd04033af7d817224e6b791dfb4c8e760
Vinod J <vinodj.2194@gmail.com>
2025-01-02 15:52:41 +0530

    completed maincontainer

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,13 +1,13 @@
 const VideoTitle = ({title, description}) => {
   return (
-    <div className='pt-36 px-12'>
+    <div className='w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-6xl font-bold'>{title}</h1>
         <p className='py-6 text-lg w-1/4'>{description}</p>
         <div>
-            <button className='bg-gray-500 text-white text-xl px-12 p-4 bg-opacity-50 rounded-lg'>
-               ▶️ Play
+            <button className='bg-white text-black text-xl px-12 p-4 rounded-lg hover:bg-opacity-60'>
+                Play
             </button>
-            <button className='bg-gray-500 text-white text-xl px-12 p-4 bg-opacity-50 rounded-lg mx-2'>
+            <button className='bg-gray-500 text-white text-xl px-12 p-4 rounded-lg mx-2 hover:bg-opacity-60'>
                 More Info
             </button>
         </div>
