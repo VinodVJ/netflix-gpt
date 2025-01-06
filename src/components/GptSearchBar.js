bfd2da0fb963d8857af18fc6c3b391b9a8097f3f
Vinod J <vinodj.2194@gmail.com>
2025-01-06 09:59:15 +0530

    Added search bar

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,10 +1,14 @@
 
 const GptSearchBar = () => {
   return (
-    <div>
-        <form className="pt-[20%]">
-            <input type="text" placeholder="Write to get movie suggestions" />
-            <button className="bg-red-700 p-4 m-2 rounded-lg">Search</button>
+    <div className="pt-[10%] flex justify-center">
+        <form className=" w-1/2 bg-black grid grid-cols-12">
+            <input
+                type="text"
+                placeholder="Write to get movie suggestions" 
+                className="p-4 m-4 col-span-9"
+            />
+            <button className="bg-red-700 py-2 px-4 m-4 text-white rounded-lg col-span-3">Search</button>
         </form>
     </div>
   )
--- src/components/GptSearchPage.js
+++ src/components/GptSearchPage.js
@@ -1,9 +1,14 @@
+import { LOGIN_BG_IMG } from "../utils/constants"
 import GptMoviesSuggestion from "./GptMoviesSuggestion"
 import GptSearchBar from "./GptSearchBar"
 
 const GptSearchPage = () => {
   return (
     <div>
+        <div className='absolute -z-10'>
+            <img src={LOGIN_BG_IMG}
+                alt='background' />
+        </div>
         <GptSearchBar />
         <GptMoviesSuggestion />
     </div>
